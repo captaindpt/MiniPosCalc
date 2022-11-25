195cef12e44c4f66fd6673c2d8c406ef7f5b21a9
tkort04 <thaerkort@gmail.com>
2022-11-25 10:35:30 -0500

    added file

---
+++ fun.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+int main (){
+// distance function
+   int i;
+for(i=1; i<3; i++){
+    struct memeber Mymember;
+    struct memeber memeber[i];
+    float latm, lat, distance;
+    distance= sqrt((latm+lat)*(latm+lat));
+}
+}
